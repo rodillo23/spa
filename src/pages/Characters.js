31e061c88fc4070093b47a8e00efb535e9d986dd
Rodolfo Figueroa <ficr23@hotmail.com>
2021-05-18 12:08:20 -0500

    proyecto terminado

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,19 +1,27 @@
-const Character = ()=>{
+import getHash from '../utils/getHash'
+import getData from '../utils/getData'
+
+const Character = async ()=>{
+  
+  const id = getHash()
+  const character = await getData(id)
+  console.log(character);
+
   const view = `
     <div class="Characters-inner">
       
       <article class="Characters-card">
-        <img src="image" alt="nombre"/>
-        <h2>Nombre</h2>
+        <img src="${character.image}" alt="${character.name}"/>
+        <h2>${character.name}</h2>
       </article>
 
       <article class="Characters-card">
-        <h3>Episodes:</h3>
-        <h3>Status:</h3>
-        <h3>Species:</h3>
-        <h3>Gender:</h3>
-        <h3>Origin:</h3>
-        <h3>Last Location:</h3>
+        <h3>Episodes: <span>${character.episode.length}</span></h3>
+        <h3>Status: ${character.status}</h3>
+        <h3>Species: ${character.species}</h3>
+        <h3>Gender: ${character.gender}</h3>
+        <h3>Origin: ${character.origin['name']}</h3>
+        <h3>Last Location: ${character.location['name']}</h3>
       </article>
       
     </div>
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,7 @@
 const API = 'https://rickandmortyapi.com/api/character'
 
 const getData = async (id)=>{
-  const apiURL = id ? `${API}${id}` : API
+  const apiURL = id ? `${API}/${id}` : API
 
   try {
     const response = await fetch(apiURL)
